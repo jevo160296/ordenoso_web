c259b65530b69878dd11b41613b9ef7442accda9
Jose Miguel Millan <jevo160296@gmail.com>
2024-05-21 10:17:17 -0500

    Add use restaurants

--- src/data/DataModel.tsx
+++ src/data/DataModel.tsx
@@ -1,12 +1,18 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Person } from "../ui/person";
+import { Restaurant } from "../ui/restaurant";
 
 const url = "https://us-central1-helloktor-422701.cloudfunctions.net"
 
 export function usePersons(): Array<Person>{
     const {data} = useAxios(`${url}/get_persons?cant=10`)
     return data
+}
+
+export function useRestaurants(): Array<Restaurant>{
+  const {data} = useAxios(`${url}/get_restaurants?cant=10`)
+  return data
 }
 
 export function useAxios(url:string): {data: any, loading:any, error:any}{
