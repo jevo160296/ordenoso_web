cc5c5c9555d43201411b1582ff18fbe610999a5e
Jose Miguel Millan <jevo160296@gmail.com>
2024-05-21 21:46:01 -0500

    Build login page

--- src/ui/pages/LoginPage.tsx
+++ src/ui/pages/LoginPage.tsx
@@ -1,7 +1,34 @@
+import { Box, Button, Paper, Stack, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+
 export default function LoginPage(){
+    const [email, setEmail] = useState<string | null>(null);
+    const [password, setPassword] = useState<string | null>(null);
+    const [canLogin, setCanLogin] = useState<boolean>(!!email && !!password);
+    useEffect(() => {setCanLogin(!!email && !!password)},[email, password])
     return (
-        <div>
-            Login page
-        </div>
+        <Box sx={{
+                display: "flex",
+                height: "100vh",
+                alignItems: "center",
+                justifyContent: "center"
+            }}>
+                <Paper 
+                    elevation={8}
+                    sx={{
+                        padding: "24px"
+                    }}
+                    >
+                <Stack spacing={2}>
+                    <p>Iniciar sesión</p>
+                    <TextField variant="outlined" label="Email" type="email" value={email} onChange={(event) => setEmail(event.target.value)}/>
+                    <TextField variant="outlined" label="Password" type="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
+                    <Stack direction="row" spacing={4}>
+                        <Button variant="contained" disabled={!canLogin}>Login</Button>
+                        <Button variant="outlined">Create account</Button>
+                    </Stack>
+                </Stack>
+                </Paper>
+        </Box>
     )
 }
